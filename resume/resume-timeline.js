b7538be5f4febad892589b3b451f9376752f1eaa
Thomas Smallwood <tbm.smallwood@gmail.com>
2021-12-15 22:04:20 +0100

    Filled in content, styling details, added category selector

--- resume/resume-timeline.js
+++ resume/resume-timeline.js
@@ -1,9 +1,12 @@
 const root = document.querySelector(":root");
 const uab = document.getElementById("uab");
 const utc = document.getElementById("utc");
+const pro = document.getElementsByClassName("professional");
+const academic = document.getElementsByClassName("academic");
 const proBasketball = document.getElementById("basketball");
 const selectTheme = document.getElementById("theme-selector");
 const selectOrder = document.getElementById("order-selector");
+const selectCategory = document.getElementById("category-selector");
 
 function changeTheme(accentColor, bgColor, boxBgColor) {
     root.style.setProperty("--boxBackgroundColor", boxBgColor);
@@ -26,13 +29,13 @@ selectTheme.addEventListener("change", () => {
             utc.style.setProperty("--accentColor", "hsla(209, 100%, 21%, 1)");
             basketball.style.setProperty(
                 "--backgroundColor",
-                "hsla(39, 100%, 34%, 0.1)"
+                "hsla(221, 100%, 14%, 0.15)"
             );
             basketball.style.setProperty("--accentColor", "hsla(0, 0%, 0%, 1)");
             break;
 
         case "dark":
-            changeTheme("white", "hsla(0, 0%, 0%, 0.3)", "rgb(70,70,70)");
+            changeTheme("white", "hsla(0, 0%, 0%, 0.6)", "rgb(70,70,70)");
             break;
         case "light":
             changeTheme("black", "hsla(0, 0%, 94%,0.3)", "white");
@@ -40,8 +43,8 @@ selectTheme.addEventListener("change", () => {
         case "vintage":
             changeTheme(
                 "hsl(165.6, 100%, 100%)",
-                "hsla(165.6, 27.78%, 64.71%, 0.3)",
-                "hsla(0, 82.76%, 65.88%, 0.7    )"
+                "hsla(165.6, 27.78%, 64.71%, 0.6)",
+                "hsla(0, 82.76%, 65.88%, 0.7)"
             );
             break;
         case "matrix":
@@ -69,6 +72,45 @@ selectOrder.addEventListener("change", () => {
             uab.style.setProperty("order", "1");
             utc.style.setProperty("order", "2");
             basketball.style.setProperty("order", "3");
+            break;
+    }
+});
+
+// selectCategory.addEventListener("change", () => {
+//     const choice = selectCategory.value;
+//     switch (choice) {
+//         case "all":
+//             pro.style.setProperty("display", "block");
+//             academic.style.setProperty("display", "block");
+//             break;
+//         case "professional":
+//             pro.style.setProperty("display", "block");
+//             academic.style.setProperty("display", "none");
+//             break;
+//         case "academic":
+//             pro.style.setProperty("display", "none");
+//             academic.style.setProperty("display", "block");
+//             break;
+//     }
+// });
+
+selectCategory.addEventListener("change", () => {
+    const choice = selectCategory.value;
+    switch (choice) {
+        case "all":
+            uab.style.setProperty("display", "block");
+            utc.style.setProperty("display", "block");
+            basketball.style.setProperty("display", "block");
+            break;
+        case "professional":
+            uab.style.setProperty("display", "none");
+            utc.style.setProperty("display", "none");
+            basketball.style.setProperty("display", "block");
+            break;
+        case "academic":
+            uab.style.setProperty("display", "block");
+            utc.style.setProperty("display", "block");
+            basketball.style.setProperty("display", "none");
             break;
     }
 });
